435b306cc55e401b85291fbf418b115f805c6538
Rafael Pichelli <rafael.pichelli4@hotmail.com>
2023-12-06 17:39:05 -0300

    Alterações no back para o final do projeto

--- src/main/java/com/pichellitech/minhasfinancas/api/controller/LancamentoController.java
+++ src/main/java/com/pichellitech/minhasfinancas/api/controller/LancamentoController.java
@@ -63,6 +63,12 @@ public class LancamentoController {
                 new ResponseEntity("Lançamento não encontrado na base de dados.", HttpStatus.BAD_REQUEST));
     }
 
+    @GetMapping("{id}")
+    public ResponseEntity obterLancamento(@PathVariable("id") Long id){
+        return service.obterPorId(id).map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity(HttpStatus.NOT_FOUND));
+    }
+
     @GetMapping
     public ResponseEntity buscar(@RequestParam(value = "descricao", required = false) String descricao,
                                  @RequestParam(value = "mes", required = false) Integer mes,
@@ -106,6 +112,19 @@ public class LancamentoController {
 
         }).orElseGet(() ->
                 new ResponseEntity<>("Lançamento não encontrado na base.", HttpStatus.BAD_REQUEST));
+    }
+
+    private LancamentoDTO converter(Lancamento lancamento){
+        return  LancamentoDTO.builder()
+                .id(lancamento.getId())
+                .descricao(lancamento.getDescricao())
+                .valor(lancamento.getValor())
+                .mes(lancamento.getMes())
+                .ano(lancamento.getAno())
+                .status(lancamento.getStatus().name())
+                .tipo(lancamento.getTipo().name())
+                .usuario(lancamento.getUsuario().getId())
+                .build();
     }
 
     private Lancamento converter(LancamentoDTO dto){
--- src/main/java/com/pichellitech/minhasfinancas/model/repository/LancamentoRepository.java
+++ src/main/java/com/pichellitech/minhasfinancas/model/repository/LancamentoRepository.java
@@ -1,6 +1,7 @@
 package com.pichellitech.minhasfinancas.model.repository;
 
 import com.pichellitech.minhasfinancas.model.entity.Lancamento;
+import com.pichellitech.minhasfinancas.model.enums.StatusLancamento;
 import com.pichellitech.minhasfinancas.model.enums.TipoLancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,8 +10,9 @@ import org.springframework.data.repository.query.Param;
 import java.math.BigDecimal;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento,Long> {
-    @Query("SELECT SUM(l.valor) FROM Lancamento l WHERE l.usuario.id = :idUsuario AND l.tipo = :tipo")
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(
+    @Query("SELECT SUM(l.valor) FROM Lancamento l WHERE l.usuario.id = :idUsuario AND l.tipo = :tipo AND l.status = :status")
+    BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
             @Param("idUsuario") Long idUsuario,
-            @Param("tipo") TipoLancamento tipo);
+            @Param("tipo") TipoLancamento tipo,
+            @Param("status") StatusLancamento status);
 }
